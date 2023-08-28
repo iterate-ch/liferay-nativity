1a7ec2411c2fceea302a5418c223d77721bd4f99
AliveDevil <AliveDevil@users.noreply.github.com>
2023-08-28 16:03:45 +0200

    Update build-script. Allow fetching version from Pom Add switch to skip build or install

--- build.ps1
+++ build.ps1
@@ -1,17 +1,30 @@
 Param(
-	[Parameter(Mandatory=$true)] [String] $version
+    [Switch] $skipBuild,
+    [Switch] $skipInstall,
+	[String] $version
 )
 
-mvn clean install
-ant -f build-overlays.xml overlays
-ant -f build.xml build-windows-menus
+if (-not $version) {
+    $pom = [xml](Get-Content pom.xml)
+    $version = [String]$pom.project.version
+}
 
-foreach ($file in Get-ChildItem dist\*.dll) {
-    Write-Host "Adding $($file.BaseName)"
-    mvn install:install-file `
-        -Dfile="$($file.FullName)" `
-        -DgroupId="ch.iterate.liferay" `
-        -DartifactId="$($file.BaseName)" `
-        -Dversion="$($version)" `
-        -Dpackaging="dll" -Dclassifier="native" -DgeneratePom="true"
+Write-Host "** Building for ${version} **"
+
+if (-not $skipBuild) {
+    mvn clean install
+    ant -f build-overlays.xml overlays
+    ant -f build.xml build-windows-menus
+}
+
+if (-not $skipInstall) {
+    foreach ($file in Get-ChildItem dist\*.dll) {
+        Write-Host "Adding $($file.BaseName)"
+        mvn install:install-file `
+            -Dfile="$($file.FullName)" `
+            -DgroupId="ch.iterate.liferay" `
+            -DartifactId="$($file.BaseName)" `
+            -Dversion="${version}" `
+            -Dpackaging="dll" -Dclassifier="native" -DgeneratePom="true"
+    }
 }
